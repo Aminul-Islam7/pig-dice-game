6e9bf21f194535be1b58c11f2f982527b107bcb9
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-07-27 23:34:05 +0600

    feat: Add logic to show winner and stop game

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ let score = {
 	1: 0,
 	2: 0,
 };
+let gameOver = false;
 
 const switchPlayers = function () {
+	currentScore = 0;
+	updateCurrentScore();
 	activePlayer = activePlayer === 1 ? 2 : 1;
 	document.querySelector('.player-1').classList.toggle('player-active');
 	document.querySelector('.player-2').classList.toggle('player-active');
@@ -28,6 +31,8 @@ const updateCurrentScore = function () {
 
 // Rolling dice
 btnRollElement.addEventListener('click', function () {
+	if (gameOver) return;
+
 	// Generate a random dice roll
 	const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -37,8 +42,6 @@ btnRollElement.addEventListener('click', function () {
 
 	// Check for rolled 1
 	if (dice === 1) {
-		currentScore = 0;
-		updateCurrentScore();
 		switchPlayers();
 	} else {
 		currentScore += dice;
@@ -48,11 +51,16 @@ btnRollElement.addEventListener('click', function () {
 
 // Holding scores
 btnHoldElement.addEventListener('click', function () {
+	if (gameOver) return;
+
 	score[activePlayer] += currentScore;
 	document.getElementById(`score-${activePlayer}`).textContent = score[activePlayer];
 
-	currentScore = 0;
-	updateCurrentScore();
-	switchPlayers();
+	if (score[activePlayer] >= 100) {
+		gameOver = true;
+		diceElement.classList.add('hidden');
+		document.querySelector(`.player-${activePlayer}`).classList.add('player-winner');
+		document.querySelector(`.player-${activePlayer}`).classList.remove('player-active');
+	} else switchPlayers();
 });
 
