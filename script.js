83d9ba1955c44539ea05b8a8a74216655bb155be
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-07-27 23:15:31 +0600

    feat: Add logic to hold scores

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 'use strict';
 
 // Select elements
-const score1 = document.getElementById('score-1');
-const score2 = document.getElementById('score-2');
-const current1 = document.getElementById('current-1');
-const current2 = document.getElementById('current-2');
-const player1 = document.querySelector('.player-1');
-const player2 = document.querySelector('.player-2');
 const diceElement = document.querySelector('.dice');
 const btnNewElement = document.querySelector('.btn-new');
 const btnRollElement = document.querySelector('.btn-roll');
 const btnHoldElement = document.querySelector('.btn-hold');
 
 // Set initial conditions
-score1.textContent = 0;
-score2.textContent = 0;
 diceElement.classList.add('hidden');
 
 let currentScore = 0;
-let activePlayer = player1;
-
-const switchPlayers = function () {
-	if (activePlayer === player1) activePlayer = player2;
-	else activePlayer = player1;
-	player1.classList.toggle('player-active');
-	player2.classList.toggle('player-active');
+let activePlayer = 1;
+let score = {
+	1: 0,
+	2: 0,
 };
 
-// Rolling dice functionality
+const switchPlayers = function () {
+	activePlayer = activePlayer === 1 ? 2 : 1;
+	document.querySelector('.player-1').classList.toggle('player-active');
+	document.querySelector('.player-2').classList.toggle('player-active');
+};
+
+const updateCurrentScore = function () {
+	document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+};
+
+// Rolling dice
 btnRollElement.addEventListener('click', function () {
 	// Generate a random dice roll
 	const dice = Math.trunc(Math.random() * 6) + 1;
@@ -39,11 +38,21 @@ btnRollElement.addEventListener('click', function () {
 	// Check for rolled 1
 	if (dice === 1) {
 		currentScore = 0;
-		activePlayer.querySelector('.current-score').textContent = currentScore;
+		updateCurrentScore();
 		switchPlayers();
 	} else {
 		currentScore += dice;
-		activePlayer.querySelector('.current-score').textContent = currentScore;
+		updateCurrentScore();
 	}
+});
+
+// Holding scores
+btnHoldElement.addEventListener('click', function () {
+	score[activePlayer] += currentScore;
+	document.getElementById(`score-${activePlayer}`).textContent = score[activePlayer];
+
+	currentScore = 0;
+	updateCurrentScore();
+	switchPlayers();
 });
 
